8ca50fcee72b0a4d40e50c4ea02a48cbc1265181
takahirochiku <tik.chiku@gmail.com>
2017-07-22 09:26:11 +0900

    0722 9009:30 submit

--- app/src/main/java/jp/techacademy/chiku/takahiro/nutritionmanegment/SettingActivity.java
+++ app/src/main/java/jp/techacademy/chiku/takahiro/nutritionmanegment/SettingActivity.java
@@ -1,14 +1,108 @@
 package jp.techacademy.chiku.takahiro.nutritionmanegment;
 
+import android.annotation.SuppressLint;
+import android.graphics.RectF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
 
-public class SettingActivity extends AppCompatActivity {
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.MPPointF;
+
+import java.util.ArrayList;
+
+public class SettingActivity extends AppCompatActivity implements OnChartValueSelectedListener {
+
+    protected HorizontalBarChart mProteinChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         setTitle("設定");
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mProteinChart = (HorizontalBarChart) findViewById(R.id.protein_chart);
+        mProteinChart.setOnChartValueSelectedListener(this);
+        // mChart.setHighlightEnabled(false);
+
+        XAxis xl = mProteinChart.getXAxis();
+        xl.setPosition(XAxis.XAxisPosition.BOTTOM);
+        //xl.setTypeface(mTfLight);
+        xl.setDrawAxisLine(true);
+        xl.setDrawGridLines(false);
+        xl.setGranularity(10f);
+
+        YAxis yl = mProteinChart.getAxisLeft();
+        //yl.setTypeface(mTfLight);
+        yl.setDrawAxisLine(true);
+        yl.setDrawGridLines(true);
+        yl.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+//        yl.setInverted(true);
+
+        YAxis yr = mProteinChart.getAxisRight();
+        //yr.setTypeface(mTfLight);
+        yr.setDrawAxisLine(true);
+        yr.setDrawGridLines(false);
+        yr.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+//        yr.setInverted(true);
+
+        setData(12, 50);
+        mProteinChart.setFitBars(true);
+        mProteinChart.animateY(2500);
     }
+
+    private void setData(int count, float range) {
+
+        float barWidth = 9f;
+        float spaceForBar = 10f;
+        ArrayList<BarEntry> proteinchart = new ArrayList<BarEntry>();
+
+        proteinchart.add(new BarEntry(4f, 0));
+
+        BarDataSet set1;
+
+        if (mProteinChart.getData() != null &&
+                mProteinChart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet)mProteinChart.getData().getDataSetByIndex(0);
+            set1.setValues(proteinchart);
+            mProteinChart.getData().notifyDataChanged();
+            mProteinChart.notifyDataSetChanged();
+        } else {
+            Log.d("TESTEST","チャートに入れる数値がありません");
+        }
+    }
+    protected RectF mOnValueSelectedRectF = new RectF();
+    @SuppressLint("NewApi")
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+        if (e == null)
+            return;
+
+        RectF bounds = mOnValueSelectedRectF;
+        mProteinChart.getBarBounds((BarEntry) e, bounds);
+
+        MPPointF position = mProteinChart.getPosition(e, mProteinChart.getData().getDataSetByIndex(h.getDataSetIndex())
+                .getAxisDependency());
+
+        Log.i("bounds", bounds.toString());
+        Log.i("position", position.toString());
+
+        MPPointF.recycleInstance(position);
+    }
+
+    @Override
+    public void onNothingSelected() {
+    };
 }
