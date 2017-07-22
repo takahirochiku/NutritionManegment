5df96a4d559658123c6415b6290f3f3ba655917a
takahirochiku <tik.chiku@gmail.com>
2017-07-22 10:45:18 +0900

    0722 10:45 submit

--- app/src/main/java/jp/techacademy/chiku/takahiro/nutritionmanegment/SettingActivity.java
+++ app/src/main/java/jp/techacademy/chiku/takahiro/nutritionmanegment/SettingActivity.java
@@ -8,11 +8,14 @@ import android.util.Log;
 import android.view.WindowManager;
 
 import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.MPPointF;
@@ -36,17 +39,30 @@ public class SettingActivity extends AppCompatActivity implements OnChartValueSe
         mProteinChart.setOnChartValueSelectedListener(this);
         // mChart.setHighlightEnabled(false);
 
+        //意味を確認する
+        mProteinChart.setDrawBarShadow(false);
+        mProteinChart.setDrawValueAboveBar(false);
+        mProteinChart.setPinchZoom(false);
+        mProteinChart.setDrawGridBackground(false);
+
+        mProteinChart.getDescription().setEnabled(false);
+
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("Protein");
+
         XAxis xl = mProteinChart.getXAxis();
         xl.setPosition(XAxis.XAxisPosition.BOTTOM);
         //xl.setTypeface(mTfLight);
         xl.setDrawAxisLine(true);
-        xl.setDrawGridLines(false);
-        xl.setGranularity(10f);
+        xl.setDrawGridLines(true);
+        CategoryBarChartXaxisFormatter xaxisFormatter = new CategoryBarChartXaxisFormatter(labels);
+        xl.setValueFormatter(xaxisFormatter);
+        xl.setGranularity(1);
 
         YAxis yl = mProteinChart.getAxisLeft();
         //yl.setTypeface(mTfLight);
-        yl.setDrawAxisLine(true);
-        yl.setDrawGridLines(true);
+        yl.setDrawAxisLine(false);
+        yl.setDrawGridLines(false);
         yl.setAxisMinimum(0f); // this replaces setStartAtZero(true)
 //        yl.setInverted(true);
 
@@ -57,32 +73,38 @@ public class SettingActivity extends AppCompatActivity implements OnChartValueSe
         yr.setAxisMinimum(0f); // this replaces setStartAtZero(true)
 //        yr.setInverted(true);
 
-        setData(12, 50);
+        BarData data = new BarData(getDataSet());
+        mProteinChart.setData(data);
         mProteinChart.setFitBars(true);
         mProteinChart.animateY(2500);
+        mProteinChart.invalidate();
+
     }
 
-    private void setData(int count, float range) {
-
-        float barWidth = 9f;
-        float spaceForBar = 10f;
+    private BarDataSet getDataSet() {
+        //float barWidth = 9f;
+        //float spaceForBar = 10f;
         ArrayList<BarEntry> proteinchart = new ArrayList<BarEntry>();
 
-        proteinchart.add(new BarEntry(4f, 0));
+        proteinchart.add(new BarEntry(1f, 100));
 
         BarDataSet set1;
 
         if (mProteinChart.getData() != null &&
                 mProteinChart.getData().getDataSetCount() > 0) {
-            set1 = (BarDataSet)mProteinChart.getData().getDataSetByIndex(0);
+            set1 = (BarDataSet) mProteinChart.getData().getDataSetByIndex(0);
             set1.setValues(proteinchart);
             mProteinChart.getData().notifyDataChanged();
             mProteinChart.notifyDataSetChanged();
         } else {
-            Log.d("TESTEST","チャートに入れる数値がありません");
+            Log.d("TESTEST", "チャートに入れる数値がありません");
         }
+        BarDataSet dataset = new BarDataSet(proteinchart, "Protein");
+        return dataset;
     }
+
     protected RectF mOnValueSelectedRectF = new RectF();
+
     @SuppressLint("NewApi")
     @Override
     public void onValueSelected(Entry e, Highlight h) {
@@ -104,5 +126,27 @@ public class SettingActivity extends AppCompatActivity implements OnChartValueSe
 
     @Override
     public void onNothingSelected() {
-    };
+    }
+
+    public class CategoryBarChartXaxisFormatter implements IAxisValueFormatter {
+
+        ArrayList<String> mValues;
+
+        public CategoryBarChartXaxisFormatter(ArrayList<String> values) {
+            this.mValues = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+
+            int val = (int) value;
+            String label = "";
+            if (val >= 0 && val < mValues.size()) {
+                label = mValues.get(val);
+            } else {
+                label = "";
+            }
+            return label;
+        }
+    }
 }
