d240ad2b42d7c113d355a26934bb4aed5b7702d5
takahirochiku <tik.chiku@gmail.com>
2017-07-22 20:59:14 +0900

    0722 21:00 submit

--- app/src/main/java/jp/techacademy/chiku/takahiro/nutritionmanegment/MainActivity.java
+++ app/src/main/java/jp/techacademy/chiku/takahiro/nutritionmanegment/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.techacademy.chiku.takahiro.nutritionmanegment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -15,9 +18,24 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.MPPointF;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements OnChartValueSelectedListener {
 
     private Toolbar mToolbar;
+    protected HorizontalBarChart mProteinChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +43,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        setTitle("");
+
+        mProteinChart = (HorizontalBarChart) findViewById(R.id.protein_chart2);
+        mProteinChart.setOnChartValueSelectedListener(this);
+        // mChart.setHighlightEnabled(false);
+
+        //意味を確認する
+        mProteinChart.setDrawBarShadow(false);
+        mProteinChart.setDrawValueAboveBar(true);
+        mProteinChart.setPinchZoom(false);
+        mProteinChart.setDrawGridBackground(false);
+        mProteinChart.getDescription().setEnabled(false);
+
+        XAxis xl = mProteinChart.getXAxis();
+        //xl.setTypeface(mTfLight);
+        xl.setDrawAxisLine(false);
+        xl.setDrawGridLines(false);
+        xl.setGranularity(20f);
+
+        YAxis yl = mProteinChart.getAxisLeft();
+        //yl.setTypeface(mTfLight);
+        yl.setDrawAxisLine(false);
+        yl.setDrawGridLines(false);
+        yl.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+//        yl.setInverted(true);
+
+        YAxis yr = mProteinChart.getAxisRight();
+        yr.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+        //yr.setTypeface(mTfLight);
+        yr.setDrawAxisLine(false);
+        yr.setDrawGridLines(false);
+        yr.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+//        yr.setInverted(true);
+
+        Legend l = mProteinChart.getLegend();
+        //l.setFormSize(5f); // set the size of the legend forms/shapes
+        l.setForm(Legend.LegendForm.SQUARE); // set what type of form/shape should be used
+        l.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+        //l.setTypeface(...);
+        l.setTextSize(12f);
+        l.setTextColor(Color.BLACK);
+        l.setXEntrySpace(5f); // set the space between the legend entries on the x-axis
+        l.setYEntrySpace(5f); // set the space between the legend entries on the y-axis
+
+        BarData data = new BarData(getDataSet());
+        mProteinChart.setData(data);
+        mProteinChart.setFitBars(false);
+        mProteinChart.animateY(2500);
+        mProteinChart.invalidate();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +135,51 @@ public class MainActivity extends AppCompatActivity {
         }
     });
 }
+
+    private BarDataSet getDataSet() {
+        ArrayList<BarEntry> proteinchart = new ArrayList<BarEntry>();
+
+        proteinchart.add(new BarEntry(1f, 100));
+
+        BarDataSet set1;
+
+        if (mProteinChart.getData() != null &&
+                mProteinChart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) mProteinChart.getData().getDataSetByIndex(0);
+            set1.setValues(proteinchart);
+            mProteinChart.getData().notifyDataChanged();
+            mProteinChart.notifyDataSetChanged();
+        } else {
+            Log.d("TESTEST", "チャートに入れる数値がありません");
+        }
+        BarDataSet dataset = new BarDataSet(proteinchart, "Protein");
+        return dataset;
+    }
+
+    protected RectF mOnValueSelectedRectF = new RectF();
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+        if (e == null)
+            return;
+
+        RectF bounds = mOnValueSelectedRectF;
+        mProteinChart.getBarBounds((BarEntry) e, bounds);
+
+        MPPointF position = mProteinChart.getPosition(e, mProteinChart.getData().getDataSetByIndex(h.getDataSetIndex())
+                .getAxisDependency());
+
+        Log.i("bounds", bounds.toString());
+        Log.i("position", position.toString());
+
+        MPPointF.recycleInstance(position);
+    }
+
+    @Override
+    public void onNothingSelected() {
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
